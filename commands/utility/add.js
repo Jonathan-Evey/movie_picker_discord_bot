b2d10d7c0b04692d476e3f28891fbf39b4b3f5d3
NighIsTheEnd <jonathan.c.evey@gmail.com>
2023-12-09 13:52:53 -0800

    add functions to pull out data from reply input

--- commands/utility/add.js
+++ commands/utility/add.js
@@ -6,12 +6,21 @@ module.exports = {
     .setDescription("Adds a movie to the list")
     .addStringOption((option) =>
       option
-        .setName("input")
+        .setName("title")
         .setDescription("Add your movie title here")
         .setRequired(true)
     ),
   async execute(interaction) {
-    console.log("send data");
-    await interaction.reply({ content: "Movie added!", ephemeral: true });
+    console.log(interaction);
+    let serverId = interaction.member.guild.id;
+    console.log(serverId);
+    let userId = interaction.user.id;
+    console.log(userId);
+    let movieTitle = interaction.options.get("title");
+    console.log(movieTitle);
+    await interaction.reply({
+      content: `${movieTitle.value} added!`,
+      ephemeral: true,
+    });
   },
 };
