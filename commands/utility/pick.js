ab6433a17d96880bf7df8bfbae28865f46d14895
NighIsTheEnd <jonathan.c.evey@gmail.com>
2024-01-02 20:09:59 -0800

    add functions to pull movies from the db. pick a random one, and then display it to the user

--- commands/utility/pick.js
+++ commands/utility/pick.js
@@ -1,6 +1,16 @@
 const { SlashCommandBuilder } = require("discord.js");
+const mongoose = require("mongoose");
+const MovieSchema = require("../../schema/MovieSchema.js");
 
 let selectedMovie = "Movie Title Here";
+
+const pickMovie = async (db) => {
+  let movieList = await db.find({});
+  if (movieList) {
+    let pickIndexNumber = Math.floor(Math.random() * movieList.length);
+    selectedMovie = movieList[pickIndexNumber].toObject()["movie_title"];
+  }
+};
 let wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 module.exports = {
@@ -8,8 +18,12 @@ module.exports = {
     .setName("pick")
     .setDescription("Pick a movie from the server's movie list"),
   async execute(interaction) {
-    await interaction.reply("Making movie selection...");
-    await wait(2000);
+    mongoose.models = {};
+    let MovieList = mongoose.model("Movie", MovieSchema);
+    await interaction.reply({
+      content: `Making movie selection...`,
+    });
+    await pickMovie(MovieList);
     await interaction.editReply(selectedMovie);
   },
 };
