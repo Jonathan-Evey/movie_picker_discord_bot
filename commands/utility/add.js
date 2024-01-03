2487533c7887d00d40e165ac13f9b48555e40352
NighIsTheEnd <jonathan.c.evey@gmail.com>
2024-01-02 18:42:25 -0800

    add functions to check db for movie on list before adding to list

--- commands/utility/add.js
+++ commands/utility/add.js
@@ -1,4 +1,33 @@
 const { SlashCommandBuilder } = require("discord.js");
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+const MovieSchema = new Schema({
+  movie_title: String,
+  user_id: { type: Number, ref: "UserID" },
+  server_id: { type: Number, ref: "ServerID" },
+});
+
+mongoose.set("strictQuery", false);
+
+const checkForMovie = async (db, movieTitle) => {
+  let movieFound = await db.findOne({ movie_title: movieTitle });
+  console.log(movieFound);
+  if (movieFound) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const saveMovie = async (db, movieTitle, userId, serverId) => {
+  const newMovie = new db({
+    movie_title: movieTitle,
+    user_id: userId,
+    server_id: serverId,
+  });
+  await newMovie.save();
+};
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -11,16 +40,26 @@ module.exports = {
         .setRequired(true)
     ),
   async execute(interaction) {
-    console.log(interaction);
+    const Movie = mongoose.model("Movie", MovieSchema);
+    //console.log(interaction);
     let serverId = interaction.member.guild.id;
-    console.log(serverId);
+    //console.log(serverId);
     let userId = interaction.user.id;
-    console.log(userId);
+    //console.log(userId);
     let movieTitle = interaction.options.get("title");
-    console.log(movieTitle);
-    await interaction.reply({
-      content: `${movieTitle.value} added!`,
-      ephemeral: true,
-    });
+    //console.log(movieTitle);
+    let isMovie = await checkForMovie(Movie, movieTitle.value);
+    if (isMovie) {
+      interaction.reply({
+        content: `${movieTitle.value} is already added!`,
+        ephemeral: true,
+      });
+    } else {
+      await saveMovie(Movie, movieTitle.value, userId, serverId);
+      interaction.reply({
+        content: `${movieTitle.value} added!`,
+        ephemeral: true,
+      });
+    }
   },
 };
--- database/utility/add.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-// exports.add_new_movie = [
-//     (req, res, next) => {
-// 		// Extract the validation errors from a request.
-// 		const errors = validationResult(req);
-// ]
