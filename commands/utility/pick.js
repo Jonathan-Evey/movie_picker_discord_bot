b7e28bba8104101fc40fbadddde138adc3462d8e
NighIsTheEnd <jonathan.c.evey@gmail.com>
2024-01-07 08:04:26 -0800

    moved pick functions to model file

--- commands/utility/pick.js
+++ commands/utility/pick.js
@@ -1,29 +1,15 @@
 const { SlashCommandBuilder } = require("discord.js");
-const mongoose = require("mongoose");
-const MovieSchema = require("../../schema/MovieSchema.js");
-
-let selectedMovie = "Movie Title Here";
-
-const pickMovie = async (db) => {
-  let movieList = await db.find({});
-  if (movieList) {
-    let pickIndexNumber = Math.floor(Math.random() * movieList.length);
-    selectedMovie = movieList[pickIndexNumber].toObject()["movie_title"];
-  }
-};
-let wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+const { pickMovie } = require("../models/movieModule.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("pick")
     .setDescription("Pick a movie from the server's movie list"),
   async execute(interaction) {
-    mongoose.models = {};
-    let MovieList = mongoose.model("Movie", MovieSchema);
     await interaction.reply({
       content: `Making movie selection...`,
     });
-    await pickMovie(MovieList);
+    let selectedMovie = await pickMovie();
     await interaction.editReply(selectedMovie);
   },
 };
