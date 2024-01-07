8e803db04388687186d554f0a3cfd35d38b101e1
NighIsTheEnd <jonathan.c.evey@gmail.com>
2024-01-07 08:06:49 -0800

    add a movie module to hold functions for movie list db interactions

---
+++ commands/models/movieModule.js
@@ -0,0 +1,47 @@
+const Movies = require("../schemas/Movie");
+
+/// ---- add function
+const saveMovie = async (movieTitle, userId, userName, serverId) => {
+  const newMovie = new Movies({
+    movie_title: movieTitle,
+    user_id: userId,
+    user_name: userName,
+    server_id: serverId,
+  });
+  await newMovie.save();
+};
+/// ---- add function
+const checkForMovie = async (movieTitle) => {
+  let movieFound = await Movies.findOne({ movie_title: movieTitle });
+  if (movieFound) {
+    return true;
+  } else {
+    return false;
+  }
+};
+/// ---- pick function
+const pickMovie = async () => {
+  let movieList = await Movies.find({});
+  if (movieList) {
+    let pickIndexNumber = Math.floor(Math.random() * movieList.length);
+    selectedMovie = movieList[pickIndexNumber].toObject()["movie_title"];
+    movieAddedBy = movieList[pickIndexNumber].toObject()["user_name"];
+    return `${selectedMovie}\nMovie was added by ${movieAddedBy}`;
+  }
+};
+
+/// ---- list function
+const findUserMovieList = async (id) => {
+  let moviesFound = await Movies.find({ user_id: id });
+  if (moviesFound) {
+    let movieList = "";
+    moviesFound.forEach((movie) => {
+      movieList = movieList + `${movie.toObject()["movie_title"]}\n`;
+    });
+    return movieList;
+  } else {
+    return "You have no movies added on the server list.";
+  }
+};
+
+module.exports = { saveMovie, checkForMovie, pickMovie, findUserMovieList };
