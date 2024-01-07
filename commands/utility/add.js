fd7c6e2483947e209256dd8677cd819c6caaa82b
NighIsTheEnd <jonathan.c.evey@gmail.com>
2024-01-07 08:03:48 -0800

    moved functions to model file

--- commands/utility/add.js
+++ commands/utility/add.js
@@ -1,27 +1,5 @@
 const { SlashCommandBuilder } = require("discord.js");
-const mongoose = require("mongoose");
-const MovieSchema = require("../../schema/MovieSchema.js");
-
-mongoose.set("strictQuery", false);
-
-const checkForMovie = async (db, movieTitle) => {
-  let movieFound = await db.findOne({ movie_title: movieTitle });
-  console.log(movieFound);
-  if (movieFound) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-const saveMovie = async (db, movieTitle, userId, serverId) => {
-  const newMovie = new db({
-    movie_title: movieTitle,
-    user_id: userId,
-    server_id: serverId,
-  });
-  await newMovie.save();
-};
+const { saveMovie, checkForMovie } = require("../models/movieModule.js");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -34,23 +12,20 @@ module.exports = {
         .setRequired(true)
     ),
   async execute(interaction) {
-    mongoose.models = {};
-    let MovieList = mongoose.model("Movie", MovieSchema);
-    //console.log(interaction);
-    let serverId = interaction.member.guild.id;
-    //console.log(serverId);
-    let userId = interaction.user.id;
-    //console.log(userId);
+    console.log(interaction);
     let movieTitle = interaction.options.get("title");
-    //console.log(movieTitle);
-    let isMovie = await checkForMovie(MovieList, movieTitle.value);
+    let userId = interaction.user.id;
+    let userName = interaction.user.globalName;
+    let serverId = interaction.member.guild.id;
+
+    let isMovie = await checkForMovie(movieTitle.value);
     if (isMovie) {
       interaction.reply({
         content: `${movieTitle.value} is already added!`,
         ephemeral: true,
       });
     } else {
-      await saveMovie(MovieList, movieTitle.value, userId, serverId);
+      await saveMovie(movieTitle.value, userId, userName, serverId);
       interaction.reply({
         content: `${movieTitle.value} added!`,
         ephemeral: true,
