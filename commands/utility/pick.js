4c5017b6da26fc537dbe3a1a65f4574dd68063a9
NighIsTheEnd <jonathan.c.evey@gmail.com>
2024-01-15 12:02:06 -0800

    started to work on a yes no button for the user to say if they watch the movie

--- commands/utility/pick.js
+++ commands/utility/pick.js
@@ -1,4 +1,9 @@
-const { SlashCommandBuilder } = require("discord.js");
+const {
+  SlashCommandBuilder,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+} = require("discord.js");
 const { pickMovie } = require("../models/movieModule.js");
 
 module.exports = {
@@ -6,10 +11,23 @@ module.exports = {
     .setName("pick")
     .setDescription("Pick a movie from the server's movie list"),
   async execute(interaction) {
-    await interaction.reply({
-      content: `Making movie selection...`,
-    });
+    const yesButton = new ButtonBuilder()
+      .setCustomId("yesButton")
+      .setLabel("Yes")
+      .setStyle(ButtonStyle.Success);
+    const noButton = new ButtonBuilder()
+      .setCustomId("noButton")
+      .setLabel("No")
+      .setStyle(ButtonStyle.Secondary);
+    const row = new ActionRowBuilder().addComponents(yesButton, noButton);
+    console.log(row);
     let selectedMovie = await pickMovie();
-    await interaction.editReply(selectedMovie);
+    await interaction.reply({
+      content: `${selectedMovie}\nAre you going to watch this movie?`,
+      components: [row],
+    });
+    // await interaction.update({
+
+    // })
   },
 };
