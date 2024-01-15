468847152e6c97879b26652b5be33420acf34ff9
NighIsTheEnd <jonathan.c.evey@gmail.com>
2024-01-15 14:53:01 -0800

    update to add a user name check

--- commands/utility/add.js
+++ commands/utility/add.js
@@ -2,34 +2,43 @@ const { SlashCommandBuilder } = require("discord.js");
 const { saveMovie, checkForMovie } = require("../models/movieModule.js");
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("add")
-    .setDescription("Adds a movie to the list")
-    .addStringOption((option) =>
-      option
-        .setName("title")
-        .setDescription("Add your movie title here")
-        .setRequired(true)
-    ),
-  async execute(interaction) {
-    console.log(interaction);
-    let movieTitle = interaction.options.get("title");
-    let userId = interaction.user.id;
-    let userName = interaction.user.globalName;
-    let serverId = interaction.member.guild.id;
+	data: new SlashCommandBuilder()
+		.setName("add")
+		.setDescription("Adds a movie to the list")
+		.addStringOption((option) =>
+			option
+				.setName("title")
+				.setDescription("Add your movie title here")
+				.setRequired(true)
+		),
+	async execute(interaction) {
+		console.log(interaction);
+		let movieTitle = interaction.options.get("title");
+		let userId = interaction.user.id;
+		let userName;
+		console.log(interaction.member.nickname);
+		if (interaction.member.nickname) {
+			userName = interaction.member.nickname;
+		} else if (interaction.user.globalName) {
+			userName = interaction.user.globalName;
+		} else {
+			userName = "Display name not set up";
+		}
 
-    let isMovie = await checkForMovie(movieTitle.value);
-    if (isMovie) {
-      interaction.reply({
-        content: `${movieTitle.value} is already added!`,
-        ephemeral: true,
-      });
-    } else {
-      await saveMovie(movieTitle.value, userId, userName, serverId);
-      interaction.reply({
-        content: `${movieTitle.value} added!`,
-        ephemeral: true,
-      });
-    }
-  },
+		let serverId = interaction.member.guild.id;
+
+		let isMovie = await checkForMovie(movieTitle.value);
+		if (isMovie) {
+			interaction.reply({
+				content: `${movieTitle.value} is already added!`,
+				ephemeral: true,
+			});
+		} else {
+			await saveMovie(movieTitle.value, userId, userName, serverId);
+			interaction.reply({
+				content: `${movieTitle.value} added!`,
+				ephemeral: true,
+			});
+		}
+	},
 };
