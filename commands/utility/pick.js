695567c4474d3c1ee91d0767f005a68012ef3ba6
NighIsTheEnd <jonathan.c.evey@gmail.com>
2024-01-15 14:53:42 -0800

    finished pick command

--- commands/utility/pick.js
+++ commands/utility/pick.js
@@ -1,33 +1,61 @@
 const {
-  SlashCommandBuilder,
-  ActionRowBuilder,
-  ButtonBuilder,
-  ButtonStyle,
+	SlashCommandBuilder,
+	ActionRowBuilder,
+	ButtonBuilder,
+	ButtonStyle,
 } = require("discord.js");
-const { pickMovie } = require("../models/movieModule.js");
+const { pickMovie, updateMovieToWatched } = require("../models/movieModule.js");
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("pick")
-    .setDescription("Pick a movie from the server's movie list"),
-  async execute(interaction) {
-    const yesButton = new ButtonBuilder()
-      .setCustomId("yesButton")
-      .setLabel("Yes")
-      .setStyle(ButtonStyle.Success);
-    const noButton = new ButtonBuilder()
-      .setCustomId("noButton")
-      .setLabel("No")
-      .setStyle(ButtonStyle.Secondary);
-    const row = new ActionRowBuilder().addComponents(yesButton, noButton);
-    console.log(row);
-    let selectedMovie = await pickMovie();
-    await interaction.reply({
-      content: `${selectedMovie}\nAre you going to watch this movie?`,
-      components: [row],
-    });
-    // await interaction.update({
+	data: new SlashCommandBuilder()
+		.setName("pick")
+		.setDescription("Pick a movie from the server's movie list"),
+	async execute(interaction) {
+		const yesButton = new ButtonBuilder()
+			.setCustomId("yesButton")
+			.setLabel("Yes")
+			.setStyle(ButtonStyle.Success);
+		const noButton = new ButtonBuilder()
+			.setCustomId("noButton")
+			.setLabel("No")
+			.setStyle(ButtonStyle.Secondary);
+		const row = new ActionRowBuilder().addComponents(yesButton, noButton);
+		let selectedMovie = await pickMovie();
+		if (selectedMovie === false) {
+			await interaction.reply({
+				content:
+					"There are no movies to pick on the list. To add a movie please use the /add command.",
+			});
+		} else {
+			const response = await interaction.reply({
+				content: `"${selectedMovie.title}" - added by ${selectedMovie.addedBy}\nAre you going to watch this movie?`,
+				components: [row],
+			});
 
-    // })
-  },
+			try {
+				const confirmation = await response.awaitMessageComponent({
+					time: 60_000,
+				});
+				if (confirmation.customId === "yesButton") {
+					await updateMovieToWatched(selectedMovie.title);
+					await interaction.editReply({
+						content: `${selectedMovie.title} has been added to the watched list, enjoy your movie!`,
+						components: [],
+					});
+				} else if (confirmation.customId === "noButton") {
+					await interaction.editReply({
+						content:
+							"Movie has been put back on the list, please us /pick to pick a new movie.",
+						components: [],
+					});
+				}
+			} catch (error) {
+				await interaction.editReply({
+					content:
+						"Confirmation not received within 5 minutes, please call /pick again to pick a new movie.",
+					components: [],
+				});
+			}
+		}
+	},
 };
