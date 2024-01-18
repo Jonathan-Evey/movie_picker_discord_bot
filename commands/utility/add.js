c654bf22e9b5542169254345c36ec74fa4584d3e
NighIsTheEnd <jonathan.c.evey@gmail.com>
2024-01-17 19:51:23 -0800

    add functions to format titles from users, and lets a movie get added if it is on the watched list

--- commands/utility/add.js
+++ commands/utility/add.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require("discord.js");
 const { saveMovie, checkForMovie } = require("../models/movieModule.js");
+const { formatTitle } = require("./formatTitle");
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -12,31 +13,30 @@ module.exports = {
 				.setRequired(true)
 		),
 	async execute(interaction) {
-		console.log(interaction);
 		let movieTitle = interaction.options.get("title");
-		let userId = interaction.user.id;
-		let userName;
-		console.log(interaction.member.nickname);
-		if (interaction.member.nickname) {
-			userName = interaction.member.nickname;
-		} else if (interaction.user.globalName) {
-			userName = interaction.user.globalName;
-		} else {
-			userName = "Display name not set up";
-		}
-
-		let serverId = interaction.member.guild.id;
-
-		let isMovie = await checkForMovie(movieTitle.value);
+		let titleFormatted = formatTitle(movieTitle.value);
+		let isMovie = await checkForMovie(titleFormatted);
 		if (isMovie) {
 			interaction.reply({
-				content: `${movieTitle.value} is already added!`,
+				content: `${titleFormatted} is already added!`,
 				ephemeral: true,
 			});
 		} else {
-			await saveMovie(movieTitle.value, userId, userName, serverId);
+			let userId = interaction.user.id;
+			let userName;
+			if (interaction.member.nickname) {
+				userName = interaction.member.nickname;
+			} else if (interaction.user.globalName) {
+				userName = interaction.user.globalName;
+			} else {
+				userName = "Display name not set up";
+			}
+
+			let serverId = interaction.member.guild.id;
+
+			await saveMovie(titleFormatted, userId, userName, serverId);
 			interaction.reply({
-				content: `${movieTitle.value} added!`,
+				content: `${titleFormatted} added!`,
 				ephemeral: true,
 			});
 		}
